7219354b6a1eb98e09aa27c87f4386f98cd5ec1a
Luiz Mendonça <luizeduardomendonca77@gmail.com>
2026-01-21 21:10:32 -0300

    add rotas, arquitetura

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,13 @@
 import cors from 'cors';
 import 'dotenv/config';
 import express from 'express';
+import { router } from './routes';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json())
+app.use(router)
 
 const PORT = process.env.PORT || 3333;
 
---
+++ src/services/user/CreateUserService.ts
@@ -0,0 +1,9 @@
+class CreateUserService{
+    async execute(){
+        console.log("serviço sendo execultado");
+
+        return "Usuário criado com sucesso";
+}
+}
+
+export {CreateUserService}
