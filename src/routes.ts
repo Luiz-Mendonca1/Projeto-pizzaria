e3dbcdd315209765d7263ade6ace2a3e63ee6b8c
Luiz Mendonça <luizeduardomendonca77@gmail.com>
2026-01-22 10:38:48 -0300

    add validação

--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,10 @@
 import { Router} from "express";
 import { CreateUserController } from "./controllers/users/CreateUserControler";
+import { validateSchema } from "./middlewares/validateSchemas";
+import { CreateUserSchema } from "./schemas/userSchema";
 
  const router = Router();
 
- router.post("/users", new CreateUserController().handle);
+ router.post("/users", validateSchema(CreateUserSchema), new CreateUserController().handle);
 
  export {router}
