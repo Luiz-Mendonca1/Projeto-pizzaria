1a926d8317020a91f864213296ec5f0eabcafa39
Luiz Mendonça <luizeduardomendonca77@gmail.com>
2026-01-26 19:55:26 -0300

    chore prisma/index.ts

--- src/prisma/index.ts
+++ src/prisma/index.ts
@@ -1,12 +1,19 @@
 import "dotenv/config";
+import { Pool } from "pg"; 
+import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "../generated/prisma/client";
-import {PrismaPg} from '@prisma/adapter-pg'; 
 
-const connectionString = `${process.env.DATABASE_URL!}`
-const adapter = new PrismaPg({connectionString});
+const connectionString = `${process.env.DATABASE_URL!}`;
 
-const PrismaClient = new PrismaClient({
+// 1. Configurar o Pool do Postgres
+const pool = new Pool({ connectionString });
+
+// 2. Configurar o Adapter
+const adapter = new PrismaPg(pool);
+
+// 3. Instancia o prismaClient 
+const prismaClient = new PrismaClient({
   adapter,
 });
 
-export default PrismaClient;
+export default prismaClient;
