5689f7ed5ec63219b4e3b7245e92bc8d504ee437
Luiz Mendonça <luizeduardomendonca77@gmail.com>
2026-01-30 19:46:59 -0300

    feat criptografia de senha

--- src/services/user/CreateUserService.ts
+++ src/services/user/CreateUserService.ts
@@ -1,23 +1,36 @@
-import prismaClient from "../../prisma";
-interface CreateUserProps{
-    name: string;
-    email: string;
-    password: string;
+import prismaClient from "../../prisma/index";
+import { hash } from "bcryptjs";
+
+interface CreateUserProps {
+  name: string;
+  email: string;
+  password: string;
 }
 
-class CreateUserService{
-    async execute({name, email, password}: CreateUserProps){
+class CreateUserService {
+  async execute({ name, email, password }: CreateUserProps) {
+    const userAlreadyExists = await prismaClient.user.findFirst({
+      where: {
+        email: email,
+      },
+    });
 
-        const user = await prismaClient.user.create({
-            data:{
-                name: name,
-                email: email,
-                password: password,
-            }
-        });
+    if (userAlreadyExists) {
+      throw new Error("Usuário já existente!");
+    }
 
-        return user.name;
+    const passwordHash = await hash(password, 8);
+
+    const user = await prismaClient.user.create({
+      data: {
+        name: name,
+        email: email,
+        password: passwordHash,
+      },
+    });
+
+    return user.name;
+  }
 }
-}
 
-export {CreateUserService}
+export { CreateUserService };
